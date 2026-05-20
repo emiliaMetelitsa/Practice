61e78024cb456809b609c7c5528be7fcaf54e842
emiliaMetelitsa <klthsrswm@gmail.com>
2026-05-20 19:44:42 +0300

    Update main.cpp

--- ProducerConsumer/main.cpp
+++ ProducerConsumer/main.cpp
@@ -1,61 +1,8 @@
-#include <iostream>
-#include <queue>
-#include <functional>
-#include <chrono>
-#include <mutex>
+#include <memory>
 #include <thread>
-#include <chrono>
-
-std::mutex m;
-
-class Queue {
-
-	std::mutex m;
-
-public:
-
-	std::queue<std::function<void()>> q;
-
-	void Push(std::function<void()> f) {
-		std::lock_guard<std::mutex> lock(m);
-		q.push(f);
-	}
-	std::function<void()> Pop() {
-		std::lock_guard<std::mutex> lock(m);
-		auto op = q.front();
-		q.pop();
-		return op;
-	}
-};
-
-class Producer {
-
-	int i = 0;
-
-public:
-
-	void Run(std::shared_ptr<Queue> queue) {
-		while (true) {
-			int cur_i = i;
-			queue->Push([cur_i]() {std::cout << "Iteration:" << cur_i << std::endl;});
-			i++;
-		}
-	}
-};
-
-class Consumer {
-
-public:
-
-	void Run(std::shared_ptr<Queue> queue) {
-		while (true) {
-			if (!queue->q.empty()) {
-				auto op = queue->Pop();
-				op();
-			}
-		}
-	}
-};
+#include "Queue.h"
+#include "Producer.h"
+#include "Consumer.h"
 
 int main() {
 	std::shared_ptr<Queue> p = std::make_shared<Queue>();
