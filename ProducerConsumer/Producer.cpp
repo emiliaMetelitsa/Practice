c5549c51da3e4a5e022a1741aac84035b334706f
emiliaMetelitsa <klthsrswm@gmail.com>
2026-05-22 11:29:39 +0300

    Update Producer.cpp

--- ProducerConsumer/Producer.cpp
+++ ProducerConsumer/Producer.cpp
@@ -3,9 +3,11 @@
 
 void Producer::Run(std::shared_ptr<Queue> queue) {
 
+    int cur_i;
+
     while (true) {
 
-        int cur_i = i;
+        cur_i = i;
 
         queue->Push([cur_i]() {
             std::cout << "Iteration: " << cur_i << std::endl;
